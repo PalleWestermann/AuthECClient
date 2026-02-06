7d1663e2ef6a8abb64e19c971071008d945c7af6
Palle Westermann <pwe@tec.dk>
2026-02-06 10:33:51 +0100

    Initial SignUp Form Done

--- src/app/user/registration/registration.ts
+++ src/app/user/registration/registration.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
 })
 export class Registration {
+
+  form: any;
+
+    constructor(public formBuilder: FormBuilder){
+      this.form = this.formBuilder.group({
+        fullName : [''],
+        email : [''],
+        password : [''],
+        confirmPassword : ['']
+      });
+
+    }
+
+  onSubmit() {
+    console.log(this.form.value);
+
+  }
 
 }
